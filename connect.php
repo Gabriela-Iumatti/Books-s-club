21fe093c6d2129b1505bbba27c5c43e997499e63
Gabriela-Iumatti <67278324+Gabriela-Iumatti@users.noreply.github.com>
2021-05-24 06:04:29 -0300

    Create connect.php
    Account file

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+//defined path
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_register.sql";
+$dbname2 = "db_delete.sql";
+$dbname3 = "db_edit.sql";
+$dbname4 = "db_reserve.sql";
+
+//created conection
+$conn =mysqli_connect($servername, $username, $password, $dbname, $dbname2, $dbname3, $dbname4);
+?>
